ad236fa557774de3823026de0732a3ee089e3f12
Raigardas <raigardas.tautkus@knf.stud.vu.lt>
2022-12-04 18:30:49 +0200

    fixed eslint

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -5,17 +5,29 @@ import { FaFacebookF } from 'react-icons/fa'
 const SocialMedia = () => {
   return (
     <div className='app__social'>
-      <a href='https://github.com/betadeveloper' target='_blank'>
+      <a
+        href='https://github.com/betadeveloper'
+        target='_blank'
+        rel='noreferrer'
+      >
         <div>
           <BsGithub />
         </div>
       </a>
-      <a href='https://www.facebook.com/eRaigardasTautkus/' target='_blank'>
+      <a
+        href='https://www.facebook.com/eRaigardasTautkus/'
+        target='_blank'
+        rel='noreferrer'
+      >
         <div>
           <FaFacebookF />
         </div>
       </a>
-      <a href='https://www.linkedin.com/in/raigardas-tautkus/' target='_blank'>
+      <a
+        href='https://www.linkedin.com/in/raigardas-tautkus/'
+        target='_blank'
+        rel='noreferrer'
+      >
         <div>
           <BsLinkedin />
         </div>
