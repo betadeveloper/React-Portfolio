5c0aefc6c6a6dcf4717fcb2604238832db157a95
Raigardas <raigardas.tautkus@knf.stud.vu.lt>
2022-12-22 04:42:21 +0200

    added parallax to header

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { Parallax } from 'react-parallax'
+import headerimg from './headerimg.jpg'
 
 import { images } from '../../constants'
 import './Header.scss'
@@ -16,54 +18,62 @@ const scaleVariants = {
 }
 
 const Header = () => (
-  <div className='app__header app__flex' id='home'>
-    <motion.div
-      whileInView={{ x: [-100, 0], opacity: [0, 1] }}
-      transition={{ duration: 0.5 }}
-      className='app__header-info'
-    >
-      <div className='app__header-badge'>
-        <div className='badge-cmp app__flex'>
-          <span>👋</span>
-          <div style={{ marginLeft: 20 }}>
-            <p className='p-text'>Hello, I am</p>
-            <h1 className='head-text'>Raigardas</h1>
+  <Parallax
+    bgImage={headerimg}
+    bgImageAlt='the cat'
+    strength={800}
+    className='img'
+    blur={1}
+  >
+    <div className='app__header app__flex' id='home'>
+      <motion.div
+        whileInView={{ x: [-100, 0], opacity: [0, 1] }}
+        transition={{ duration: 0.5 }}
+        className='app__header-info'
+      >
+        <div className='app__header-badge'>
+          <div className='badge-cmp app__flex'>
+            <span>👋</span>
+            <div style={{ marginLeft: 20 }}>
+              <p className='p-text'>Hello, I am</p>
+              <h1 className='head-text'>Raigardas</h1>
+            </div>
+          </div>
+
+          <div className='tag-cmp app__flex'>
+            <p className='p-text'>An Aspiring Web Developer</p>
           </div>
         </div>
+      </motion.div>
 
-        <div className='tag-cmp app__flex'>
-          <p className='p-text'>An Aspiring Web Developer</p>
-        </div>
-      </div>
-    </motion.div>
+      <motion.div
+        whileInView={{ opacity: [0, 1] }}
+        transition={{ duration: 0.5, delayChildren: 0.5 }}
+        className='app__header-img'
+      >
+        <img src={images.profilesvg} alt='profile_bg' />
+        <motion.img
+          whileInView={{ scale: [0, 1] }}
+          transition={{ duration: 1, ease: 'easeInOut' }}
+          src={images.circle}
+          alt='profile_circle'
+          className='overlay_circle'
+        />
+      </motion.div>
 
-    <motion.div
-      whileInView={{ opacity: [0, 1] }}
-      transition={{ duration: 0.5, delayChildren: 0.5 }}
-      className='app__header-img'
-    >
-      <img src={images.profilesvg} alt='profile_bg' />
-      <motion.img
-        whileInView={{ scale: [0, 1] }}
-        transition={{ duration: 1, ease: 'easeInOut' }}
-        src={images.circle}
-        alt='profile_circle'
-        className='overlay_circle'
-      />
-    </motion.div>
-
-    <motion.div
-      variants={scaleVariants}
-      whileInView={scaleVariants.whileInView}
-      className='app__header-circles'
-    >
-      {[images.typescript, images.react, images.sass].map((circle, index) => (
-        <div className='circle-cmp app__flex' key={`circle-${index}`}>
-          <img src={circle} alt='profile_bg' />
-        </div>
-      ))}
-    </motion.div>
-  </div>
+      <motion.div
+        variants={scaleVariants}
+        whileInView={scaleVariants.whileInView}
+        className='app__header-circles'
+      >
+        {[images.typescript, images.react, images.sass].map((circle, index) => (
+          <div className='circle-cmp app__flex' key={`circle-${index}`}>
+            <img src={circle} alt='profile_bg' />
+          </div>
+        ))}
+      </motion.div>
+    </div>
+  </Parallax>
 )
 
 export default Header
